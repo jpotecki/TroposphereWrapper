4fac46f1c7ea65b1b5369b2db06ccbcf4ae1067d
Janos Potecki <janos.potecki.15@ucl.ac.uk>
2017-09-28 17:05:13 +0200

    Change: Add more features to `iam.py` - now working

--- troposphereWrapper/iam.py
+++ troposphereWrapper/iam.py
@@ -1,5 +1,5 @@
-from troposphere import Template, Ref
-from troposphere.iam import Role, InstanceProfile
+from troposphere import Template, Ref, Sub
+from troposphere.iam import Role, InstanceProfile, Policy
 
 import awacs.aws
 import awacs.s3
@@ -15,6 +15,7 @@ import awacs.cloudwatch
 import awacs.cloudformation
 import awacs.rds
 import awacs.sqs
+import awacs.sts
 import awacs.ecs
 import awacs.iam
 import awacs.awslambda
@@ -34,28 +35,135 @@ class Effects(Enum):
   def get(self):
     return self.value[1]
 
+
+
 class RoleBuilder:
   def __init__(self):
     self._name: str = None
-    self._policy: awacs.aws.Policy = None
+    self._policy: List[Policy] = []
+    self._assumePolicy: awacs.aws.Policy = None
   
   def setName(self, name: str):
     self._name = name
     return self
 
-  def setPolicy(self, policy: awacs.aws.Policy):
-    self._policy = policy
+  def setAssumePolicy(self, assume: awacs.aws.Policy):
+    self._assumePolicy = assume
+    return self
+
+  def addPolicy(self, policy: Policy):
+    self._policy.append(policy)
     return self
 
   def build(self) -> Role:
     checkForNoneValues(self)
     return Role(
         self._name
-      , AssumeRolePolicyDocument = self._policy
+      , RoleName = Sub(self._name+"-${AWS::StackName}")
+      , AssumeRolePolicyDocument = self._assumePolicy
+      , Policies = self._policy
       )
 
+
+
+class StatementBuilder:
+  def __init__(self):
+    self._principal: awacs.aws.Principal = None
+    self._actions: List[ awacs.aws.Action] = []
+    self._effect: Effects = None
+    self._resource: List[str] = []
+
+  def addResource(self, res: str):
+    self._resource.append(res)
+    return self
+
+  def setPrincipal(self, principal: awacs.aws.Principal):
+    self._principal = principal
+    return self
+
+  def addAction(self, action: awacs.aws.Action):
+    self._actions.append(action)
+    return self
+
+  def setEffect(self, effect: Effects):
+    self._effect = effect
+    return self
+
+  def build(self) -> awacs.aws.Statement:
+    if self._principal is not None:
+      return awacs.aws.Statement(
+          Principal = self._principal
+        , Action = self._actions
+        , Effect = self._effect.get()
+        )
+    else:
+      return awacs.aws.Statement(
+          Action = self._actions
+        , Effect = self._effect.get()
+        , Resource = self._resource
+        )
+
+
+
+class PolicyBuilder:
+  def __init__(self):
+    self._name: str = None
+    self._statements: List[awacs.aws.Statement] = []
+  
+  def setName(self, name: str):
+    self._name = name
+    return self
+
+  def addStatement(self, doc: awacs.aws.Statement):
+    self._statements.append(doc)
+    return self
+
+  def build(self) -> Policy:
+    checkForNoneValues(self)
+    policyDocument = PolicyDocumentBuilder()
+    for s in self._statements:
+      policyDocument.addStatement(s)
+    return Policy(
+        PolicyName = Sub(self._name + "-${AWS::StackName}")
+      , PolicyDocument = policyDocument.build()
+      )
+
+
+
+class PolicyDocumentBuilder:
+  def __init__(self):
+    self._statements: List[awacs.aws.Statement] = []
+  
+  def addStatement(self, statement: awacs.aws.Statement):
+    self._statements.append(statement)
+    return self
+  
+  def build(self) -> awacs.aws.Policy:
+    return awacs.aws.Policy( Statement = self._statements )
+
+
+
 class RoleBuilderHelper:
-  def oneClickCodePipeServicePolicy(self) -> List[awacs.aws.Policy]:
+  def defaultAssumeRolePolicyDocument(self, service: str) -> awacs.aws.Policy:
+    return PolicyDocumentBuilder() \
+      .addStatement(
+        StatementBuilder() \
+          .setEffect(Effects.Allow) \
+          .setPrincipal(awacs.aws.Principal("Service", service)) \
+          .addAction(awacs.sts.AssumeRole) \
+          .build()
+        ) \
+      .build()
+    # return awacs.aws.Policy(
+    #   Statement = [ awacs.aws.Statement( 
+    #         Principal = awacs.aws.Principal("Service", service)
+    #       , Action = [ awacs.sts.AssumeRole]
+    #       , Effect = awacs.aws.Allow
+    #       )
+    #     ]
+    #   )
+
+  def oneClickCodePipeServicePolicy(self) -> Policy:
     statements = [
         awacs.aws.Statement(
           Action = [ awacs.s3.GetObject
@@ -153,18 +261,24 @@ class RoleBuilderHelper:
       , Effect = awacs.aws.Allow
       )
     ]
-    return awacs.aws.Policy(
+    policyDoc = awacs.aws.Policy(
         Statement = statements
       , Version = "2012-10-17"
+      )
+    return Policy(
+        PolicyName = Sub("oneClickCodePipeServicePolicy-${AWS::StackName}")
+      , PolicyDocument = policyDoc
       )
 
 
 def getExample() -> str:
   t = Template()
-  
+
   role = RoleBuilder() \
     .setName("ExamplePipelineRole") \
-    .setPolicy(RoleBuilderHelper().oneClickCodePipeServicePolicy()) \
+    .setAssumePolicy(RoleBuilderHelper() \
+        .defaultAssumeRolePolicyDocument("codepipeline.amazonaws.com")) \
+    .addPolicy(RoleBuilderHelper().oneClickCodePipeServicePolicy()) \
     .build()
   
   t.add_resource(role)
