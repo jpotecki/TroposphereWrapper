787f85dea9d6f5aa30f34f055c9e875a5a55cd18
Janos Potecki <janospotecki@eduroam-int-dhcp-97-176-250.ucl.ac.uk>
2017-10-17 12:08:41 +0100

    Add `setup.py`

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='troposphereWrapper',
+      version='0.1',
+      description='Wrapper for Troposphere',
+      author='Janos Potecki',
+      url='https://github.com/jpotecki/TroposphereWrapper',
+      packages=['troposphereWrapper'],
+     )
