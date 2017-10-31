b49ddb4faf6c4405dfbe365a3627e8bff55a15be
Janos Potecki <janos.potecki.15@ucl.ac.uk>
2017-10-31 15:05:21 +0100

    Add dependencies for auto. install

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@
 from distutils.core import setup
 
 setup(name='troposphereWrapper',
-      version='0.1.1',
-      description='Wrapper for Troposphere',
-      author='Janos Potecki',
-      url='https://github.com/jpotecki/TroposphereWrapper',
-      packages=['troposphereWrapper'],
+      version = '0.1.2',
+      description = 'Wrapper for Troposphere',
+      author = 'Janos Potecki',
+      url = 'https://github.com/jpotecki/TroposphereWrapper',
+      packages = ['troposphereWrapper'],
+      install_requires = [
+          'troposphere', 'awacs'
+      ]
      )
